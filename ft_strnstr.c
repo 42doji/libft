be61074464bf2a7821d828e27da701a873ce67ee
42doji <doji@student.42gyeongsan.kr>
2025-01-28 23:46:58 +0900

    header

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,24 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doji <doji@student.42gyeongsan.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 23:41:52 by doji              #+#    #+#             */
+/*   Updated: 2025/01/28 23:42:03 by doji             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
-
-// char	*ft_strnstr(const char *big, const char *little, size_t len)
-// {
-// 	size_t	i;
-// 	size_t	j;
-
-// 	if (!little && len < 1)
-// 		return ((char *)big);
-// 	i = 0;
-// 	while (big[i])
-// 	{
-// 		j = 0;
-// 		while (little[j] && big[i + j] == little[j] && i + j < len)
-// 			++j;
-// 		if (little[j] == 0)
-// 			return ((char *)(big + i));
-// 		++i;
-// 	}
-// 	return (NULL);
-// }
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
